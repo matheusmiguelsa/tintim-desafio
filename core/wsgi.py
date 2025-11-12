1ca26c5409ade07cb615f9b0391688a4e66a6412
Matheus Miguel <matheusmiguelsa03@gmail.com>
2025-11-12 12:43:21 -0300

    test

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
-# caminho do seu projeto
-path = '/home/matheusmiguelsa/tintim-desafio'
-if path not in sys.path:
-    sys.path.append(path)
+# caminho absoluto do projeto (com hífen)
+project_path = '/home/matheusmiguelsa/tintim-desafio'
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
 
+# pasta onde está o manage.py e core/settings.py
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 from django.core.wsgi import get_wsgi_application
