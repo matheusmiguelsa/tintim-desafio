189dae7cabb79a04fd4b2a4d8730284458f1047c
Matheus Miguel <matheusmiguelsa03@gmail.com>
2025-11-12 12:24:26 -0300

    test

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,4 +1,12 @@
 import os
+import sys
+
+# caminho do seu projeto
+path = '/home/matheusmiguelsa/tintim-desafio'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
